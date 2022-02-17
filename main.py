a51c6e9edc31d7c2b80179518254b3e06671653f
Benjamin Sadik <benjaminsadik@google.com>
2022-02-17 13:15:28 +0000

    finish generate url function

--- main.py
+++ main.py
@@ -158,9 +158,17 @@ def create_final_view(VIEW_PROJECT,
     )
 
 def generate_datastudio_url(
-
+    dashboard_id, 
+    alias_connection,
+    project_id, 
+    dataset_id, 
+    table_id,
+    new_dashboard_name = "MyAdvancedCarbonFootprintDashboard"
     ):
-    return
+
+    base_url=f"https://datastudio.google.com/reporting/create?c.reportId={dashboard_id}&r.reportName={new_dashboard_name}"
+    parameters_url = f"&ds.{alias_connection}.connector=bigQuery&ds.{alias_connection}.projectId={project_id}&ds.{alias_connection}.type=TABLE&ds.{alias_connection}.datasetId={dataset_id}&ds.{alias_connection}.tableId={table_id}"
+    return base_url+parameters_url
 
 def main(argv):
     parser=argparse.ArgumentParser(
@@ -251,3 +259,13 @@ def main(argv):
   #  "carbon_footprint_export", 
    # "USD"
 #)
+
+print(
+    generate_datastudio_url(
+        dashboard_id, 
+        alias_connection, 
+        project_id, 
+        dataset_id, 
+        "test"
+    )
+)
